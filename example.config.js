cbca55278103ba3ee3fe600725dcd0edf7477b5a
Vincent <meijer.vincent@gmail.com>
2017-09-08 19:25:11 -0400

    DEV-363 Add separate config file for example. Made it load synchronously.

---
+++ example.config.js
@@ -0,0 +1,14 @@
+require.config({
+    paths: {
+        'spidergl': 'bower_components/3d-hop/minimal/js/spidergl',
+        'jquery': 'bower_components/3d-hop/minimal/js/jquery',
+        'nexus': 'bower_components/3d-hop/minimal/js/nexus',
+        'presenter-3dhop': 'bower_components/3d-hop/minimal/js/presenter',
+        'ply': 'bower_components/3d-hop/minimal/js/ply',
+        'trackball-sphere': 'bower_components/3d-hop/minimal/js/trackball_sphere',
+        'trackball-turntable': 'bower_components/3d-hop/minimal/js/trackball_turntable',
+        'trackball-turntable-pan': 'bower_components/3d-hop/minimal/js/trackball_turntable_pan',
+        'trackball-pantilt': 'bower_components/3d-hop/minimal/js/trackball_pantilt',
+        'init-3dhop': 'bower_components/3d-hop/minimal/js/init'
+    }
+});
